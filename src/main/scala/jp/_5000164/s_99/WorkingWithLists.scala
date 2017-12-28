bec7571abafb7f184355cbefd72cf2c152ae1883
Hiroshi Sugawara <5000164@users.noreply.github.com>
2017-12-28 21:54:51 +0900

    P01: サンプルを参考に関数型プログラミングのアプローチを学ぶために再帰を使用してリストの最後の要素を取得する

--- src/main/scala/jp/_5000164/s_99/WorkingWithLists.scala
+++ src/main/scala/jp/_5000164/s_99/WorkingWithLists.scala
@@ -7,6 +7,14 @@ object WorkingWithLists extends App {
   // Find the last element of a list.
   assert(last(List(1, 1, 2, 3, 5, 8)) == 8)
   assert(last(List("a", "b", "c")) == "c")
+  assert(lastAlt(List(1, 1, 2, 3, 5, 8)) == 8)
+  assert(lastAlt(List("a", "b", "c")) == "c")
 
   def last[A](list: List[A]): A = list.last
+
+  def lastAlt[A](list: List[A]): A = list match {
+    case h :: Nil => h
+    case _ :: tail => lastAlt(tail)
+    case _ => throw new NoSuchElementException
+  }
 }
